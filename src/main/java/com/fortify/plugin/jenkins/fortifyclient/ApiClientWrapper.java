f458b7035b47bdb9d109297d9fd4565ea08cc1c9
akaryakina <akarjakina@gmail.com>
2021-04-30 11:33:19 -0700

    Reverting code changes following the redeployed dependency artifact

--- src/main/java/com/fortify/plugin/jenkins/fortifyclient/ApiClientWrapper.java
+++ src/main/java/com/fortify/plugin/jenkins/fortifyclient/ApiClientWrapper.java
@@ -268,9 +268,9 @@ public class ApiClientWrapper {
 
 		ApiResultListProjectVersionIssue apiResultListProjectVersionIssue = issueSetOfProjectVersionControllerApi
 				// .listIssueOfProjectVersion(parentId, start, limit, q, qm, orderby, filterset, fields, 
-				// showhidden, showremoved, showsuppressed, showshortfilenames, filter, groupid, groupingtype, ids, progressListener, progressRequestListene)
+				// showhidden, showremoved, showsuppressed, showshortfilenames, filter, groupid, groupingtype, /*ids, progressListener, progressRequestListene*/)
 				.listIssueOfProjectVersion(appVersionId, startPage, pageSize, null, null, "issueName", null, null,
-						false, false, false, true, filter, null, null, null, null, null);
+						false, false, false, true, filter, null, null/*, null, null, null*/);
 		for (ProjectVersionIssue issue : apiResultListProjectVersionIssue.getData()) {
 			issues.add(issue);
 		}
